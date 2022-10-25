628ec044b9da28300b61d8124f797a2e24c483cd
Kennis <kennytorrez@gmail.com>
2022-10-25 16:40:36 -0300

    primer commit de Api crud

---
+++ src/main/java/com/kennis/backendtutorial/BackendtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.kennis.backendtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kennis/backendtutorial/BackendtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennis.backendtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
